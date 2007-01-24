c40a5dade4c87bcf6d30280529fea10804fa6427
dune73 <dune73@66f2fec4-b327-0410-9e50-95d008ef093b>
2007-01-24 15:03:07 +0000

    requests migration executed

--- trunk/remo/db/migrate/001_create_requests.rb
+++ trunk/remo/db/migrate/001_create_requests.rb
@@ -1,8 +1,8 @@
 class CreateRequests < ActiveRecord::Migration
   def self.up
     create_table :requests do |t|
-      t.column  :url	:string
-      t.column	:method :string
+      t.column  :url,    :string
+      t.column  :method, :string
     end
   end
 
