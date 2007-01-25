7205c21c8b2179003f68429b62746fe94684bd51
dune73 <dune73@66f2fec4-b327-0410-9e50-95d008ef093b>
2007-01-25 13:02:30 +0000

    added script/overall-testsuite.rb

---
+++ trunk/remo/script/overall-testsuite.rb
@@ -0,0 +1,10 @@
+#!/usr/bin/env ruby
+#
+# Overall testsuite for project remo
+#
+
+require "test/unit"
+
+# testcases below are started automatically
+require "./test/functional/main_controller_test"
+require "./test/unit/request_test"
