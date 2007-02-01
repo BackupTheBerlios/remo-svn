d2018191eee1572021951687e94a84c2d2d8a0ad
dune73 <dune73@66f2fec4-b327-0410-9e50-95d008ef093b>
2007-02-01 18:07:00 +0000

    commented ./views/main/display_detailarea.rjs

--- trunk/remo/app/views/main/lib/display.rb
+++ trunk/remo/app/views/main/lib/display.rb
@@ -1,16 +1,14 @@
 def unselect_listitem (page, base_classname)
-	selector = "." + base_classname + "-selected"
-	classname_selected = base_classname + "-selected"
-	page.select(selector).each do |item|	# loop over elements matching class
-		page.call 'Element.addClassName', item, base_classname
-		page.call 'Element.removeClassName', item, classname_selected
-	end
-
+  selector = "." + base_classname + "-selected"
+  classname_selected = base_classname + "-selected"
+  page.select(selector).each do |item|	# loop over elements matching class
+    page.call 'Element.addClassName', item, base_classname
+    page.call 'Element.removeClassName', item, classname_selected
+  end
 end
 
 def select_listitem (page, item, base_classname)
-	classname_selected = base_classname + "-selected"
-	page.call 'Element.addClassName', item, classname_selected
-	page.call 'Element.removeClassName', item, base_classname
-
+  classname_selected = base_classname + "-selected"
+  page.call 'Element.addClassName', item, classname_selected
+  page.call 'Element.removeClassName', item, base_classname
 end
