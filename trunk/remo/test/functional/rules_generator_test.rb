6c88ac436b50d7536a3206d8bb0b3509f814ead8
dune73 <dune73@66f2fec4-b327-0410-9e50-95d008ef093b>
2007-02-14 14:15:26 +0000

    testing for locationmatch ruleset

--- trunk/remo/test/functional/rules_generator_test.rb
+++ trunk/remo/test/functional/rules_generator_test.rb
@@ -17,7 +17,7 @@ def get_startline(rules_array, path)
   path_hit = nil
 
   rules_array.size.times do |i|
-    regexp = /#{escaped_path}/
+    regexp = /LocationMatch.*#{escape_path(path)}/
     if regexp.match(rules_array[i])
       path_hit = i  # we hit the path on this line on the ruleset
       break
@@ -30,7 +30,7 @@ def get_startline(rules_array, path)
   start_rule = 0
   until i == 0 do
     regexp = /^(#|$)/
-    start_rule = i unless regexp.match(rules_array[i])  # counting down start_rule until we hit the comments
+    start_rule = i if regexp.match(rules_array[i]).nil?  # counting down start_rule until we hit the comments
     regexp = /^$/
     if regexp.match(rules_array[i])
       start_line = i + 1  # i is the empty line
@@ -52,24 +52,34 @@ class RulesGeneratorTest < Test::Unit::TestCase
     Request.find(:all).each do |item|
       start, start_rule = get_startline(rules_array, item.path)
 
+      # Example rule should look as follows:
+
+      #   # Basic get request
+      #   # allow: GET /info.html
+      #   <LocationMatch "^/info.html$">
+      #     SecRule REQUEST_METHOD "^GET$" "allow,t:none,nolog,id:7"
+      #   </LocationMatch>
+
       # start + 0 has to be remarks field
       assert_equal false, /^#/.match(rules_array[start]).nil?, "First line of rule is not a comment" 
       assert_equal false, /^# #{item.remarks}/.match(rules_array[start]).nil?, "First line of comment does not match the remarks field" 
-
-      # start + 1 has to be comment outlining path
+      # start + 1 has to be the comment outlining the path
       assert_equal false, /^#/.match(rules_array[start]).nil?, "First line of rule is not a comment" 
       assert_equal false, /^# allow/.match(rules_array[start + 1]).nil?, "Second line of comment does not start with 'allow'" 
 
-      # start_rule has to be the start of the rule and the method
-      regexp = /SecRule REQUEST_METHOD "\^#{item.http_method}\$" "chain,allow,nolog,id:#{item.id}"/
-      assert_equal false, regexp.match(rules_array[start_rule]).nil?, "First line of rule is not correct" 
+      # start_rule has to be the start of the rule and thus the LocationMatch
+      regexp = /^<LocationMatch "\^#{item.path}\$">$/
+      assert_equal false, regexp.match(rules_array[start_rule]).nil?, "LocationMatch line is not correct" 
 
-      # start_rule + 1 has to be the path
-      regexp = /REQUEST_URI "#{escape_path(escape_path(item.path))}"/ # double escaping it
-      assert_equal false, regexp.match(rules_array[start_rule+1]).nil?, "Second line of rule is not correct" 
+      # start_rule + 1 has to be the http_method
+      regexp = /^  SecRule REQUEST_METHOD "\^#{item.http_method}\$" "allow,t:none,nolog,id:#{item.id}"$/ 
+      assert_equal false, regexp.match(rules_array[start_rule+1]).nil?, "http method line of rule is not correct" 
 
-      # start_rule + 2 has to be empty
-      assert_equal false, /^$/.match(rules_array[start_rule+2]).nil?, "Third line of rule (delimiter) of rule is not empty" 
+      # start_rule + 2 has to be the end of LocationMatch
+      assert_equal false, /^<\/LocationMatch>$/.match(rules_array[start_rule+2]).nil?, "End of LocationMatch not found as expected" 
+
+      # start_rule + 3 has to be the end of LocationMatch
+      assert_equal false, /^$/.match(rules_array[start_rule+3]).nil?, "Fourth line of rule (delimiter) of rule is not empty" 
 
     end
 
